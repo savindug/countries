bd33b987e2d2e903e76073b96097fa0aee063761
Savindu Gunawardena <savibashnon11@gmail.com>
2021-02-11 22:27:22 +0530

    Display Api data in tabular view

--- ng-client/src/app/app.module.ts
+++ ng-client/src/app/app.module.ts
@@ -7,7 +7,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { AboutComponent } from './components/layout/about/about.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
 import { CountriesListComponent } from './components/countries-list/countries-list.component';
 
 @NgModule({
@@ -22,7 +23,8 @@ import { CountriesListComponent } from './components/countries-list/countries-li
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatTableModule       
+    MatTableModule,
+    MatIconModule       
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ng-client/src/app/components/countries-list/countries-list.component.ts
+++ ng-client/src/app/components/countries-list/countries-list.component.ts
@@ -1,6 +1,7 @@
 import { CountryService } from './../../services/country.service';
 import { Country } from './../../models/country.model';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-countries-list',
@@ -9,19 +10,26 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class CountriesListComponent implements OnInit {
 
-  @Input()
   countries!: Country[];
 
+  displayedColumns: string[] = ["id", "name", "region", "currency", "countryCode", "updated_at", "actions"];
+  dataSource = new MatTableDataSource<Country>();
 
   constructor(private countryService: CountryService) { }
 
+
   ngOnInit(): void {
-    this.getCountries()
+    this.countryService.getCountries().subscribe(_countries => {
+      this.countries = _countries
+      this.dataSource.data = this.countries;
+      console.log(JSON.stringify(this.countries))
+    })
   }
 
   getCountries = () => {
     this.countryService.getCountries().subscribe(_countries => {
       this.countries = _countries
+      this.dataSource.data = this.countries;
       console.log(JSON.stringify(this.countries))
     })
   }
