2a03c70a9ca9b18a7fa0b1f14beb1d5b3ed1862d
Savindu Gunawardena <savibashnon11@gmail.com>
2021-02-13 13:29:53 +0530

    Merge branch 'ng-client'

---
+++ ng-client/src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { AboutComponent } from './components/layout/about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFooterCell, MatTableModule } from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import { CountriesListComponent } from './components/countries-list/countries-list.component';
+import { MatButtonModule } from '@angular/material/button';
+import { AddCountryComponent } from './components/add-country/add-country.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { EditCountryComponent } from './components/edit-country/edit-country.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CountriesListComponent,
+    HeaderComponent,
+    AboutComponent,
+    AddCountryComponent,
+    EditCountryComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatSortModule,
+    FormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatInputModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ng-client/src/app/components/edit-country/edit-country.component.ts
@@ -0,0 +1,75 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Country } from 'src/app/models/country.model';
+
+interface Region {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-edit-country',
+  templateUrl: './edit-country.component.html',
+  styleUrls: ['./edit-country.component.css']
+})
+export class EditCountryComponent implements OnInit {
+
+  country!: Country;
+
+  regions: Region[] = [
+    {value: 'Africa', viewValue: 'Africa'},
+    {value: 'Asia', viewValue: 'Asia'},
+    {value: 'Caribbean', viewValue: 'Caribbean'},
+    {value: 'Central America', viewValue: 'Central America'},
+    {value: 'Europe', viewValue: 'Europe'},
+    {value: 'North America', viewValue: 'North America'},
+    {value: 'Oceania', viewValue: 'Oceania'},
+    {value: 'South America', viewValue: 'South America'}
+  ];
+
+  formGroup: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    region: new FormControl('', [Validators.required]),
+    currency: new FormControl('', [Validators.required]),
+    c_code: new FormControl('', [Validators.required])
+  });
+
+  constructor(
+    public dialogRef: MatDialogRef<EditCountryComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Country) {
+      this.country = data;
+    }
+
+  ngOnInit(): void {
+    this.formGroup.controls['name'].setValue(this.country.name);
+    this.formGroup.controls['region'].setValue(this.country.region);
+    this.formGroup.controls['currency'].setValue(this.country.currency);
+    this.formGroup.controls['c_code'].setValue(this.country.countryCode);
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onOkClick(): void {
+    this.dialogRef.close(this.country);
+  }
+
+  updateCountry() {
+    if(this.formGroup.valid){
+      console.log('selected region ', this.formGroup.controls['region'].value)
+        //this.country = new Country(, , this.formGroup.controls['currency'].value, this.formGroup.controls['c_code'].value);
+        this.country.name = (this.formGroup.controls['name'].value);
+        this.country.region = (this.formGroup.controls['region'].value);
+        this.country.currency = (this.formGroup.controls['currency'].value);
+        this.country.countryCode = (this.formGroup.controls['c_code'].value);
+        this.onOkClick();
+    }else{
+      alert('Please Provide Required Details')
+    }
+  }
+
+
+
+}
